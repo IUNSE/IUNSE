b935239e8160b43a560cb7a94ecb32171d46b1bb
IUNSE <45304842+IUNSE@users.noreply.github.com>
2018-11-24 18:09:15 +0530

    committed.
    ∆¶ init.sh, shell script to --/initialize/-- whole "IUNSE®" in one go.
    Usage: $ sh init.sh

---
+++ lib/cayman/init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# ==========================++== IUNSE® ==++==========================
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2004 - 2018, Prabhat Kumar. All rights reserved.
+# Copyright © 2014 - 2018, Sequømics Corporation, All rights reserved.
+# ====================================================================
