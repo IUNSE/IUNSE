2a70ead3afc65f663eb761cf05a3599528989072
I U N S E <45304842+IUNSE@users.noreply.github.com>
2018-11-25 10:21:09 +0530

    committed.
    ∆¶ = ( GNU General Public License, v3.0 )°• — https://github.com/IUNSE/.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,31 @@
 #!/usr/bin/env node
+/*!
+ * ------
+ * IUNSE®
+ * ------
+ * IUNSE® — International UNiversal Declarations for Sun and Earth.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 24-Nov-2018
+ * @license   : GNU General Public License, v3.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-cli
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "IUNSE®".
+ * ___________________________________________________________________________
+ */
